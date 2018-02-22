a83259404095d9272bf4a384c27781c140b918dc
Sebastian Vilhelm Nielsen <seb@frankly.dk>
2018-02-22 13:58:45 +0100

    great

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private authService: AuthService) {}
   title = 'FindASitter';
+  isLoggedIn = this.authService.isLoggedIn;
 }
