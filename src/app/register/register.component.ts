be4f8afcde56e1d5d116af2f1fc2197f57491985
Sebastian Vilhelm Nielsen <seb@frankly.dk>
2018-03-01 09:41:08 +0100

    init

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      area: ['', Validators.required],
+      gender: ['', Validators.required],
+      rate: [0, Validators.required]
+    });
+  }
+
+  onSubmit(form) {
+    /* Find current age */
+    var dob = form.value.birthDate
+    var today = new Date()
+    var birthDate = new Date(dob)
+    var age = today.getFullYear() - birthDate.getFullYear()
+    var m = today.getMonth() - birthDate.getMonth()
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--
+    }
+    console.log(age);
+    if(form.valid) {
+      // Send a request
+    } else {
+      // Error handling
+    }
+  }
 
   ngOnInit() {
   }
